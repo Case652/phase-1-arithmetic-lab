4a96518c43e156965dd14c1ca2ea91a97d90a2ed
Mateusz M Trybunia <Mateusz.m.trybunia@gmail.com>
2023-09-11 18:12:30 -0500

    Completed

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let num1 = 1
+let num2 = 62
+const multiply = (num1 * num2);
+let random = Math.floor(Math.random() * 10) + 1;
+let num3 = 4
+let num4 = 1
+const mod = (num3 / num4);
+const numbers =[1, 20, 3, 8, 5, 9, 19]
+let max = numbers[1]
+    for (let i = 1; i <numbers.length; i++) {
+        if (numbers[i] > max){
+            max = numbers[i];
+        }
+    }
